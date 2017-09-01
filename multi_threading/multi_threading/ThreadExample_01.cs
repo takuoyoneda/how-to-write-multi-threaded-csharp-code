b7d45a1b44c27d12ea50a27392d9cfa752200250
Takuo Yoneda <tyoneda@cmgames.co.jp>
2017-09-01 18:43:59 +0900

    The first commit.

---
+++ multi_threading/multi_threading/ThreadExample_01.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace multi_threading
+{
+	public class ThreadExample_01
+	{
+
+		private const int REPETITIONS = 1000;
+
+		public static void DoWork()
+		{
+			for (int i = 0; i < REPETITIONS; i++)
+			{
+				Console.Write("B");
+			}
+		}
+
+		public static void Main (string[] args)
+		{
+			Thread t1 = new Thread (new ThreadStart(DoWork));
+			t1.Start();
+
+			for(int i = 0; i < REPETITIONS; i++)
+			{
+				Console.Write("A");
+			}
+		}
+
+	}
+}
+
